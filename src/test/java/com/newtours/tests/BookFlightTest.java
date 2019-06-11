3873706eb08fe8754b78e072d16332df30cab0ea
Rane <rane@blasph.me>
2019-06-11 07:08:18 -0700

    intitial commit

---
+++ src/test/java/com/newtours/tests/BookFlightTest.java
@@ -0,0 +1,66 @@
+package com.newtours.tests;
+
+import com.newtours.pages.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class BookFlightTest {
+    private WebDriver driver;
+	private String noOfPassengers;
+	private String expectedPrice;
+
+    @BeforeTest
+    @Parameters({"noOfPassengers", "expectedPrice"})
+    public void setupDriver(String noOfPassengers, String expectedPrice) {
+        this.noOfPassengers = noOfPassengers;
+        this.expectedPrice = expectedPrice;
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rane\\Documents\\chromedriver_win32\\chromedriver.exe");
+        this.driver = new ChromeDriver();
+    }
+
+    @Test
+    public void registrationPageTest() {
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.goTo();
+        registrationPage.enterUserDetails("selenium", "docker");
+        registrationPage.enterUserCredentials("selenium", "docker");
+        registrationPage.submit();
+    }
+
+    @Test(dependsOnMethods = "registrationPageTest")
+    public void registrationConfirmationPageTest() {
+        RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
+        registrationConfirmationPage.goToFlightDetailsPage();
+    }
+
+    @Test(dependsOnMethods = "registrationConfirmationPageTest")
+    public void FlightDetailsPageTest() {
+        FlightDetailsPage flightDetailsPage = new FlightDetailsPage(driver);
+        flightDetailsPage.selectPassangers(noOfPassengers);
+        flightDetailsPage.goToFindFlightsPage();
+    }
+
+    @Test(dependsOnMethods = "FlightDetailsPageTest")
+    public void findFlightPageTest() {
+        FindFlightPage findFlightPage = new FindFlightPage(driver);
+        findFlightPage.submitFingFlightPage();
+        findFlightPage.goToFlightConfirmationPage();
+    }
+
+    @Test(dependsOnMethods = "findFlightPageTest")
+    public void flightConfirmationPageTest() {
+        FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
+        String actualPrice = flightConfirmationPage.getPrice();
+        Assert.assertEquals(actualPrice, expectedPrice);
+    }
+
+    @AfterTest
+    public void quitBrowser() {
+        this.driver.quit();
+    }
+}
